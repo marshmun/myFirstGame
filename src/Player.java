a93484aaceae0ef220cf71e305f35fd0c06207df
Marshall Munoz <Marshall.Munoz@fbitn.com>
2017-11-14 07:09:13 -0600

    player class made with documentation

--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,27 @@ public class Player {
     //variables which we will use
     private int x, y;
     private boolean left, right, up, down;
+
+    public void update(){
+        //In this function we will do the required checking and updates
+    }
+    //These 4 functions are able to set the direction
+    public void setLeft(boolean newLeft){
+        left = newLeft;
+    }public void setUp (boolean newUp){
+        up = newUp;
+    }public void setDown (boolean newDown){
+        down = newDown;
+    }public void setRight ( boolean newRight){
+        right = newRight;
+    }
+    //This function will return X as an int
+    public int getX(){
+        return x;
+    }
+    //And this function will return Y as an int
+    public int getY(){
+        return y;
+    }
+
 }
